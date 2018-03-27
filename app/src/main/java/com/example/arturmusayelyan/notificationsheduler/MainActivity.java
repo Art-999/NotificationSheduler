19686c2b192e6dd7f96258a691f96e16dbbb408e
artur.musayelyan <musayelyan099@gmail.com>
2018-03-27 19:01:42 +0400

    27.03.2018 ArtCommit gorcic ( 1 )

--- app/src/main/java/com/example/arturmusayelyan/notificationsheduler/MainActivity.java
+++ app/src/main/java/com/example/arturmusayelyan/notificationsheduler/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.arturmusayelyan.notificationsheduler;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,12 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.rg_letters)
     RadioGroup rgLetters;
+    Button defaultButton;
+    boolean buttonDefaultCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        defaultButton=findViewById(R.id.defaultButton);
+        buttonDefaultCheck=false;
 
 
         for (int i = 0; i < rgLetters.getChildCount(); i++) {
@@ -33,8 +38,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
             if (buttonView.isPressed()){
+                buttonDefaultCheck=true;
                 if (isChecked){
                     Toast.makeText(MainActivity.this, buttonView.getText(), Toast.LENGTH_SHORT).show();
+                    if(!buttonDefaultCheck){
+                        ((RadioButton)defaultButton).setChecked(false);
+                    }
                 }
             }
         }
